ad4b0e8722acf345122a4d4b47eef0edd20454de
alumno <alumno@2A5PC18>
2014-02-12 13:55:09 +0100

    carrooo

--- php/carrito.php
+++ php/carrito.php
@@ -1,37 +1,36 @@
 <?php
-$datos=$_POST["datos"];
-$carrito=stdClass();
-$carrito = json_decode($datos);
+$carritoJson=$_POST["datos"];
+$carrito= new stdClass();
+$carrito = json_decode($carritoJson);
 
-$numero = $carrito->$numero;
-$fechaCreacion = $carrito->$fechaCreacion;
-$listaArticulos = $carrito->$listaArticulos;
+$numero = $carrito->numero;//guardamos en la variable numero el contenido de numero del carrito
+$fechaCreacion = $carrito->fechaCreacion;//guadamos la fecha creacion del carrito
+$listaArticulos = $carrito->listaArticulos;//guardamos la lista de articulos del carrito
 
-$informacionPago=$_POST["informacionPago"];
-$usuario=$informacionPago->$usuario;
-$precioTotal=$informacionPago->$precioTotal;
 //insertar un pedido
 // idPedido,Fecha,Usuario,Estado,PrecioTotal
 $db=mysql_connect("localhost","root","frodo2013") or die("Connection Error".mysql_error());
 mysql_select_db("proyecto1_tienda_servidor") or die("Error Connection to DB".mysql_error());
+$SQL0 = "select max(idPedido) as maximo from pedidos where Usuario = 1;";
+$result0=mysql_query($SQL0) or die("Couldnt execute query.".mysql_error());
 
+$valorMaximo=$result0["maximo"];
+echo $valorMaximo;
 $SQL = "INSERT INTO `proyecto1_tienda_servidor`.`pedidos` 
         (`idPedido`, `Fecha`, `Usuario`, `Estado`, `PrecioTotal`) 
-VALUES (NULL,'".$fechaCreacion."','".$usuario."','ESPERA','".$listaArticulos."');";
-
+VALUES (NULL,'".$fechaCreacion."','1','ESPERA','66666');";
 $result=mysql_query($SQL) or die("Couldnt execute query.".mysql_error());
 //insertar un detallePedido
 //idPedido,IdProducto,Unidades,Precio
-foreach ($listaArticulos as &$articulo) {
-    $id=$articulo->$id;
-    $nombre=$articulo->$nombre;
-    $precio=$articulo->$precio;
-    $unidades=$articulo->$unidades;
-    
+foreach ($listaArticulos as &$articulo) {//realizar insert por cada articulo
+    $id=$articulo->id;//cogemos el id del articulo
+    $nombre=$articulo->nombre;//cogemos el nombre del articulo(no sirve para nada)
+    $precio=$articulo->precio;//cogemos el precio del articulo
+    $unidades=$articulo->unidades;//cogemos la cantidad de unidades del articulo
 $SQL2 = "INSERT INTO `proyecto1_tienda_servidor`.`detallepedidos` 
         (`idPedido`, `idProducto`, `Unidades`, `Precio`) 
-        VALUES (NULL, '".$id."', '".$unidades."', '".$precio."');";
+        VALUES ('".$valorMaximo ."', '".$id."', '".$unidades."', '".$precio."');";
 $result2=mysql_query($SQL2) or die("Couldnt execute query.".mysql_error());
 }
-
+//select max(idpedido) as maximo from pedidos where idusuario = idusuario;
 ?>
